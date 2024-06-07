2e99c9984298898a0ca093c8694d373b24920857
unknown <jpfernandes@outlook.com>
2024-06-07 16:22:28 -0300

    Atualização tela de login

--- static/footer.php
+++ static/footer.php
@@ -1,5 +1,5 @@
 <div class="footer text-center">
-    <p><b>&copy; 2023 Taverna de Valhalla. Todos os direitos reservados.</b></p>
+    <p><b>&copy; 2024 SEM NOME DEFINIDO AINDA. Todos os direitos reservados.</b></p>
     <p>Desenvolvido por João Pedro Fernandes, João Rafael, Joaquim Sagioneti, Luiz Eduardo e Matheus Vital</p>
     <p>Orientado por Dieimes Nunes de Souza</p>
 </div>
