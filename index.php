6313213e94b3bd2ae767c1392cace50972c8d108
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2024-06-20 09:52:26 -0300

    att

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Início - </title>
 </head>
 <body>
     <div class="container">
-        
+        <p id="tit">Teste CSS</p>
     </div>
 </body>
 </html>
