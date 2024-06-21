925509cb70e5e331b2f948eaf1e9a236dcc7613f
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2024-06-21 10:19:28 -0300

    Nova Atualização

--- index.php
+++ index.php
@@ -12,8 +12,26 @@
     <title>Início - </title>
 </head>
 <body>
-    <div class="container">
-        <p id="tit">Teste CSS</p>
+<div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
     </div>
+    <div class="card">
+        <div class="text-content">
+            <p>Este é um exemplo de texto onde cada palavra será traduzida em Libras ao passar o mouse.</p>
+            <p>Teste</p>
+            <br>
+            <p>Teste 2</p>
+        </div>
+        <div class="libras-doll">
+            <img src="boneco.png" alt="Boneco em Libras" id="librasImage">
+        </div>
+    </div>
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>
 </body>
 </html>
